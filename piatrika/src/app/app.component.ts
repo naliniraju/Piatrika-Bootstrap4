17e72d82b04cd147dbfd46e713dec33c1cdc5373
Nalin <s.naliniraju.gmail.com>
2019-05-04 23:21:25 +0530

    commit

--- piatrika/src/app/app.component.ts
+++ piatrika/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit,ViewEncapsulation } from '@angular/core';
 import * as $ from 'jquery'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   title = 'piatrika';
